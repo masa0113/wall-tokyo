3d07b895c684d03c36348db2dc598097f3b21795
Fukuyama a.k.a poison <69804084+masa0113@users.noreply.github.com>
2024-02-21 13:52:13 +0000

    add Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <button>クリック！</button>
+        <Button>クリック！</Button>
       </div>
     </main>
   );
